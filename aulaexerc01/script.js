beaa3c84b16d2b1ea0991a846dd7d3c66a555e65
Elisaul Velasquez <elisaulbusinesscorreo@gmail.com>
2024-07-05 14:59:50 -0300

    Mod quinta

--- aulaexerc01/script.js
+++ aulaexerc01/script.js
@@ -1,1 +1,21 @@
-var msg
+function carregar() {
+    var msg = document.getElementById("msg");
+    var img = document.getElementById("imagem");
+    var agora = new Date();
+    var hora = agora.getHours();
+    msg.innerHTML = `Agora sao ${hora} horas..`;
+  
+    if (hora >= 0 && hora < 12) {
+      //BOM DIA!
+      img.src = "manha.png";
+      document.body.style.background = "#afc9da";
+    } else if (hora >= 12 && hora < 18) {
+      //BOA TARDE!
+      img.src = "tarde.png";
+      document.body.style.background = "#ffab25";
+    } else {
+      //BOA NOITE!
+      img.src = "noite.png";
+      document.body.style.background = "#3c4a56";
+    }
+  }
